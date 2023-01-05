35ac5d62dd83371919e3800ff151e32f0a1b0cd5
Nekuromu <30809120+Nekuromu@users.noreply.github.com>
2023-01-04 22:03:49 -0600

    finished program and lesson.

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 import requests
 
-response = requests.get("https://opentdb.com/api.php", params={"amount": 10, "type": "boolean"})
+response = requests.get("https://opentdb.com/api.php", params={"amount": 10, "category": 15, "type": "boolean", "difficulty": "easy"})
 response.raise_for_status()
 question_data = response.json()["results"]
